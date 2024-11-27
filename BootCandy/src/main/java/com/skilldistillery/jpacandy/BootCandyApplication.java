8aaa96e961fab2b6da326ee5e663ea1d95da3b94
bw-deeg <idotek2@gmail.com>
2024-11-26 20:36:27 -0700

    boot

---
+++ BootCandy/src/main/java/com/skilldistillery/jpacandy/BootCandyApplication.java
@@ -0,0 +1,13 @@
+package com.skilldistillery.jpacandy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BootCandyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BootCandyApplication.class, args);
+	}
+
+}
---
+++ BootCandy/src/test/java/com/skilldistillery/jpacandy/BootCandyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.skilldistillery.jpacandy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BootCandyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
